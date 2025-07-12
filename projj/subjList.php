dadc90501ca0bfff8c4c2c9dfbaa41e1a9578e8e
EllGar <165835046+EllGar@users.noreply.github.com>
2025-07-12 12:48:37 +0300

    subjList.php

--- projj/subjList.php
+++ projj/subjList.php
@@ -58,7 +58,6 @@
   border: 1px solid rgba(78, 79, 81, 0.81);
   height: 40px;
   width: 60px;
-
   background:white;
   border-radius: 20px;
   cursor: pointer;
@@ -121,8 +120,6 @@
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
       <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6"/>
     </svg>
-    
-  </svg>
 </button>
     <button class="button">
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-backpack3" viewBox="0 0 16 16">
@@ -160,14 +157,18 @@ if (isset($data['subjects']) && is_array($data['subjects'])) {
         </tr>";
     foreach ($subjects as $subject) {
       if ($subject['student_number']==$stud_num){
+        $idSubj=$subject['id'];
         echo "<tr>
           <td>" . htmlspecialchars($subject['id']) . "</td>
           <td>" . htmlspecialchars($subject['name']) . "</td>
           <td>" . htmlspecialchars($subject['professor_surname']) . "</td>
           <td>" . htmlspecialchars($subject['status']) . "</td>
-          <td><button class='listButton'><svg xmlns='http://www.w3.org/2000/svg' width='32' height='32' fill='currentColor' class='bi bi-arrow-right-square-fill' viewBox='0 0 16 16'>
-                <path d='M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1'/>
-                </svg></button></td>
+          <td>
+            <form action='details.php' method='post'>
+            <input type='hidden' name='ia' value='$idSubj'><button class='listButton' type='submit'>
+            <svg xmlns='http://www.w3.org/2000/svg' width='32' height='32' fill='currentColor' class='bi bi-arrow-right-square-fill' viewBox='0 0 16 16'>
+              <path d='M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1'/>
+            </svg></button></form></td>
         </tr>";
     }
         echo "</table>";
@@ -185,20 +186,23 @@ if (isset($data['subjects']) && is_array($data['subjects'])) {
       
       
     foreach ($subjects as $subject) {
+      $idSubj=$subject['id'];
         echo "<tr>
           <td>" . htmlspecialchars($subject['id']) . "</td>
           <td>" . htmlspecialchars($subject['name']) . "</td>
           <td>" . htmlspecialchars($subject['professor_surname']) . "</td>
-          <td><button class='listButton'><svg xmlns='http://www.w3.org/2000/svg' width='32' height='32' fill='currentColor' class='bi bi-arrow-right-square-fill' viewBox='0 0 16 16'>
-                <path d='M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1'/>
-                </svg></button></td>
+          <td>
+            <form action='details.php' method='post'>
+            <input type='hidden' name='ia' value='$idSubj'><button class='listButton' type='submit'>
+            <svg xmlns='http://www.w3.org/2000/svg' width='32' height='32' fill='currentColor' class='bi bi-arrow-right-square-fill' viewBox='0 0 16 16'>
+              <path d='M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1'/>
+            </svg></button></form></td>
         </tr>";
     }
 
     echo "</table>";
   
 ?>
-
 
 </body>
 </html>
