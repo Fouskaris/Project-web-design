d21e5941cdfb580a02c6efba2ea82807fe5eb8cd
Drago <165834975+drago9988@users.noreply.github.com>
2025-08-05 20:46:18 +0300

    Update subjHandling.php

--- Prof/subjHandling.php
+++ Prof/subjHandling.php
@@ -1,4 +1,15 @@
 <!DOCTYPE html>
+<?php
+
+session_start();
+
+if (!isset($_SESSION['Prof_id'])) {
+    header('Location: loginScr.php');
+    exit;
+}
+$id = $_SESSION['Prof_id'];
+
+?>
 <html lang="en">
 <head>
   <meta charset="UTF-8" />
@@ -273,5 +284,6 @@ echo "</table>";
 
 </body>
 </html>
+
 
 
