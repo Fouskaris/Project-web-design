1e8e46833aedde1e8e8d97e153b5930941bb08de
Drago <165834975+drago9988@users.noreply.github.com>
2025-08-05 20:45:39 +0300

    Update subjCre.php

--- Prof/subjCre.php
+++ Prof/subjCre.php
@@ -1,4 +1,15 @@
 <!DOCTYPE html>
+<?php
+
+session_start();
+
+if (!isset($_SESSION['Prof_id'])) {
+    header('Location: loginScr.php');
+    exit;
+}
+$id = $_SESSION['Prof_id'];
+
+?>
 <html lang="en">
 <head>
 <meta charset="UTF-8" />
@@ -272,5 +283,6 @@ function addFileInput() {
 }
 </script>
 </body>
+
 
 </html>
