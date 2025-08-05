146baf57f1c411fb93ba2ea58f53b27733b1c2e0
Drago <165834975+drago9988@users.noreply.github.com>
2025-08-05 20:46:32 +0300

    Update subjStatus.php

--- Prof/subjStatus.php
+++ Prof/subjStatus.php
@@ -1,4 +1,15 @@
 <!DOCTYPE html>
+<?php
+
+session_start();
+
+if (!isset($_SESSION['Prof_id'])) {
+    header('Location: loginScr.php');
+    exit;
+}
+$id = $_SESSION['Prof_id'];
+
+?>
 <html lang="en">
 <head>
   <meta charset="UTF-8" />
@@ -313,5 +324,6 @@ foreach ($subjects as $subject) {
 
 </body>
 </html>
+
 
 
