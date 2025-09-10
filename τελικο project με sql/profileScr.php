8aceec89fa8434c000cd2cece2f861c3304be4e6
EllGar <165835046+EllGar@users.noreply.github.com>
2025-09-10 23:55:22 +0300

    Update profileScr.php

--- τελικο project με sql/profileScr.php
+++ τελικο project με sql/profileScr.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
-<html lang="en">
-  <?php
-
+<html lang="el">
+<?php
 session_start();
 
 if (!isset($_SESSION['Stud_id'])) {
@@ -10,30 +9,46 @@ if (!isset($_SESSION['Stud_id'])) {
 }
 $id = $_SESSION['Stud_id'];
 
+$json_file = 'export.json';
+$studentData = json_decode(file_get_contents($json_file), true);
+$students = &$studentData['students'];
+
+$studentFound = null;
+foreach ($students as &$student) {
+    if ($student['id'] == $id) {
+        $studentFound = &$student;
+        break;
+    }
+}
+
+$message = "";
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['student_number'], $_POST['name'], $_POST['surname'], $_POST['email'])) {
+    $student_number = trim($_POST['student_number']);
+    $new_name = trim($_POST['name']);
+    $new_surname = trim($_POST['surname']);
+    $new_email = trim($_POST['email']);
+
+    if (filter_var($new_email, FILTER_VALIDATE_EMAIL)) {
+        foreach ($students as &$student) {
+            if ($student['student_number'] == $student_number) {
+                $student['name'] = $new_name;
+                $student['surname'] = $new_surname;
+                $student['email'] = $new_email;
+                $message = "Το προφίλ ενημερώθηκε με επιτυχία.";
+                break;
+            }
+        }
+        file_put_contents($json_file, json_encode($studentData, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+    } else {
+        $message = "Μη έγκυρο email.";
+    }
+}
 ?>
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>Σύστημα Υποστήριξης Διπλωματικών Εργασιών Πανεπιστημίου Πατρών</title>
   <style>
-    body {
-      margin: 0;
-      font-family: Arial, sans-serif;
-    }
-
-    .top-menu {
-      background-color: whitesmoke;
-      color: black;
-      display: flex;
-      align-items: center;
-      padding: 10px 20px;
-      position: fixed;
-      top: 0;
-      width: 100%;
-      box-sizing: border-box;
-      z-index: 1;
-    }
-
     .menu-title {
       font-size: 30px;
     }
@@ -176,14 +191,14 @@ $id = $_SESSION['Stud_id'];
       right: 20px;
       z-index: 2;
     }
-  </style>
+    </style>
 </head>
 <body>
 
 <div class="top-menu">
   <img src="upatrasLogo.jpg" alt="Image" style="display: block; margin: 0px; width: 10em;">
   <div class="menu-title">Σύστημα Υποστήριξης Διπλωματικών Εργασιών</div>
-    <button class="buttonNotif"onclick="document.getElementById('bottom').scrollIntoView({ behavior: 'smooth' });">
+  <button class="buttonNotif" onclick="document.getElementById('bottom').scrollIntoView({ behavior: 'smooth' });">
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
       <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6"/>
     </svg>
@@ -193,46 +208,34 @@ $id = $_SESSION['Stud_id'];
 <h1 class="Pageheader">Προφίλ</h1>
 
 <?php
-$id = isset($_POST['id']) ? (int)$_POST['id'] : 0;
-$studentData = json_decode(file_get_contents("export.json"), true);
-$students = &$studentData['students'];
-$stud_num = "";
-
-// Αρχικοποίηση για να αποφύγουμε το Undefined variable
-$studentFound = null;
-
-foreach ($students as &$student) {
-    if ($student['id'] == $id) {
-      $studentFound = &$student;
-      break;
-    }
+if (!empty($message)) {
+    echo "<p style='color:green; font-weight:bold; margin-left:7em;'>$message</p>";
 }
 
 if ($studentFound) {
-      echo '<div class="card">';
-      echo '<form action="update_prof.php" method="post">';
-      echo '<input type="hidden" name="student_number" value="' . htmlspecialchars($studentFound['student_number']) . '">';
+    echo '<div class="card">';
+    echo '<form method="post">';
+    echo '<input type="hidden" name="student_number" value="' . htmlspecialchars($studentFound['student_number']) . '">';
+    
+    echo '<label><strong>Όνομα:</strong></label><br>';
+    echo '<input class="inp" type="text" name="name" value="' . htmlspecialchars($studentFound['name']) . '"><br><br>';
 
-      echo '<label><strong>Όνομα:</strong></label><br>';
-      echo '<input class="inp" type="text" name="name" value="' . htmlspecialchars($studentFound['name']) . '"><br><br>';
+    echo '<label><strong>Επίθετο:</strong></label><br>';
+    echo '<input class="inp" type="text" name="surname" value="' . htmlspecialchars($studentFound['surname']) . '"><br><br>';
 
-      echo '<label><strong>Επίθετο:</strong></label><br>';
-      echo '<input class="inp" type="text" name="surname" value="' . htmlspecialchars($studentFound['surname']) . '"><br><br>';
+    echo '<label><strong>Αριθμός Μητρώου:</strong></label><br>';
+    echo '<input class="inp" type="text" value="' . htmlspecialchars($studentFound['student_number']) . '" disabled><br><br>';
 
-      echo '<label><strong>Αριθμός Μητρώου:</strong></label><br>';
-      echo '<input class="inp" type="text" value="' . htmlspecialchars($studentFound['student_number']) . '" disabled><br><br>';
+    echo '<label><strong>Email:</strong></label><br>';
+    echo '<input class="inp" type="email" name="email" value="' . htmlspecialchars($studentFound['email']) . '"><br><br>';
 
-      echo '<label><strong>Email:</strong></label><br>';
-      echo '<input class="inp" type="email" name="email" value="' . htmlspecialchars($studentFound['email']) . '"><br><br>';
-
-      echo '<button type="submit" class="button">Αποθήκευση</button> ';
-
-      echo '</form>';
-      echo '</div>';
-  } else {
-    echo "<div class='container'><p>Δεν βρέθηκε θέμα με ID: <strong>$id</strong>.</p></div>";
-  }
-?> 
+    echo '<button type="submit" class="button">Αποθήκευση</button>';
+    echo '</form>';
+    echo '</div>';
+} else {
+    echo "<div class='container'><p>Δεν βρέθηκε φοιτητής με ID: <strong>$id</strong>.</p></div>";
+}
+?>
 
 <h1 class="Pageheader" id="bottom">Ειδοποιήσεις</h1>
 
@@ -240,37 +243,36 @@ if ($studentFound) {
 if (empty($studentFound['notifications'])) {
     echo "Δεν υπάρχουν Ειδοποιήσεις";
 } else {
-  echo '<div class="card">';
-  foreach ($studentFound["notifications"] as&$notif) {
-    if ($notif["seen"] == "no") {$notif["seen"] = "yes";}
-    $profData = json_decode(file_get_contents("export.json"), true);
-    $professors = $profData['professors'];
-  foreach ($professors as $professor) {
-    if ($professor["id"] == $notif["by"]) {
-      $sender = $professor["surname"]." ".$professor["name"];
+    echo '<div class="card">';
+    foreach ($studentFound["notifications"] as &$notif) {
+        if ($notif["seen"] == "no") {$notif["seen"] = "yes";}
+        $professors = $studentData['professors'];
+        foreach ($professors as $professor) {
+            if ($professor["id"] == $notif["by"]) {
+                $sender = $professor["surname"]." ".$professor["name"];
+            }
+        }
+        if ($notif["id"] != -1) {
+            echo "<form method='POST' action='delete_notification.php' style='display:inline;'>";  
+            echo "<h1 style='margin-top:3px;font-size: 1em;' >" . htmlspecialchars($notif["date"]) ."<strong> ". htmlspecialchars($sender) .  "</strong>:</h1> ";
+            echo htmlspecialchars($notif["message"]) . "<br>";
+            echo "<input type='hidden' name='stud_id' value='" . htmlspecialchars($id) . "'>";
+            echo "<input type='hidden' name='notif_id' value='" . htmlspecialchars($notif["id"]) . "'>";
+            echo "<button  class='delNotif'><svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='currentColor' class='bi bi-trash' viewBox='0 0 16 16'>
+            <path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z'/>
+            <path d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z'/>
+            </svg></button>";
+            echo "</form>";
+            echo "<hr style='border: 1px solid black; width: 100%;'>";
+        }
     }
-  }
-  if ($notif["id"] == -1) {}else{ 
-    echo "<form method='POST' action='delete_notification.php' style='display:inline;'>";  
-    echo "<h1 style='margin-top:3px;font-size: 1em;' >" . htmlspecialchars($notif["date"]) ."<strong>"." ". htmlspecialchars($sender) .  "</strong>:</h1> ";
-    echo htmlspecialchars($notif["message"]) . "<br>";
-    echo "<input type='hidden' name='stud_id' value='" . htmlspecialchars($id) . "'>";
-    echo "<input type='hidden' name='notif_id' value='" . htmlspecialchars($notif["id"]) . "'>";
-    echo "<button  class='delNotif'><svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='currentColor' class='bi bi-trash' viewBox='0 0 16 16'>
-    <path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z'/>
-    <path d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z'/>
-    </svg></button> ";
-    echo "</form>";
-    echo "<hr style='border: 1px solid black; width: 100%;'>";
-  }
+    echo '</div>';
 }
-  echo '</div>';
-}   
-file_put_contents("export.json", json_encode($studentData, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+
+file_put_contents($json_file, json_encode($studentData, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
 ?>
 
 <button type="button" class="button back-btn" onclick="history.back()">Επιστροφή</button>
 
 </body>
 </html>
-
